743be7ae2880eb07f7a109ba9bf0906de77156f5
Selormey Hubert <selormeyh@gmail.com>
2023-11-07 04:18:27 +0000

    Pictures

--- src/components/German.jsx
+++ src/components/German.jsx
@@ -1,54 +1,35 @@
- /* eslint-disable no-unused-vars */
+/* eslint-disable no-unused-vars */
 import React from "react";
- 
+
 import GermanCard from "../layout/GermanCard";
-import c1 from "../assets/c1.jpg";
-import graphics from "../assets/graphics.jpeg";
-import c2 from "../assets/c2.jpg";
-import c3 from "../assets/c3.jpg";
-import c4 from "../assets/c4.jpg";
-import c5 from "../assets/c5.jpg";
+import a3 from "../assets/a3.png";
+import a5 from "../assets/a5.png";
+
+import a2 from "../assets/a2.png";
+import a7 from "../assets/a7.png";
 import GermanSubtitle from "../components/GermanSubtitle";
 import CareerPath from "../components/CareerPath";
- 
-
- 
 
 const German = () => {
   return (
     <div>
-    
       <div className="bg-custom-color">
         <div className="container mx-auto p-4">
           <GermanSubtitle />
         </div>
       </div>
-       
+
       <div className="flex flex-wrap gap-6 justify-center text-black">
+        <GermanCard img={a2} title="A1" description="A1111111111111" />
+        <GermanCard img={a3} title="A2" description="A22222" />
+        <GermanCard img={a5} title="B1" description="B1111111" />
         <GermanCard
-          img={c1}
-          title="A1"
-          description="A1111111111111"
-        />
-        <GermanCard
-          img={graphics}
-          title="A2"
-          description="A22222"
-        />
-        <GermanCard
-          img={c2}
-          title="B1"
-          description="B1111111"
-        />
-        <GermanCard
-          img={c3}
+          img={a7}
           title="B2"
-          description="Phone servicing and repairs, carried out by skilled technicians with specialized tools and quality replacement parts, are vital for extending a phone's lifespan and minimizing electronic waste."
-        
+          description="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
         />
       </div>
-      <CareerPath/>
-      
+      <CareerPath />
     </div>
   );
 };
