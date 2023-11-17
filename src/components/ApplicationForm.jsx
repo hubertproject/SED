190cdb3d4926a628107f6d51a822d6477013cd71
Selormey Hubert <selormeyh@gmail.com>
2023-11-17 06:35:38 +0000

    styling

--- src/components/ApplicationForm.jsx
+++ src/components/ApplicationForm.jsx
@@ -17,93 +17,93 @@ const ApplicationForm = () => {
               <label htmlFor="firstName" className="text-sm font-medium w-1/4">
                 First Name:
               </label>
-              <input type="text" id="firstName" name="firstName" className="form-input w-3/4 text-black" />
+              <input type="text" id="firstName" name="firstName" className="form-input w-3/4 text-black  h-10" />
             </div>
 
             <div className="mb-4 flex justify-between">
               <label htmlFor="middleName" className="text-sm font-medium w-1/4">
                 Middle Name:
               </label>
-              <input type="text" id="middleName" name="middleName" className="form-input w-3/4 text-black" />
+              <input type="text" id="middleName" name="middleName" className="form-input w-3/4 text-black  h-10" />
             </div>
 
             <div className="mb-4 flex justify-between">
               <label htmlFor="lastName" className="text-sm font-medium w-1/4">
                 Last Name:
               </label>
-              <input type="text" id="lastName" name="lastName" className="form-input w-3/4 text-black" />
+              <input type="text" id="lastName" name="lastName" className="form-input w-3/4 text-black  h-10" />
             </div>
 
             <div className="mb-4 flex justify-between">
               <label htmlFor="dateOfBirth" className="text-sm font-medium w-1/4">
                 Date of Birth:
               </label>
-              <input type="date" id="dateOfBirth" name="dateOfBirth" className="form-input w-3/4 text-black" />
+              <input type="date" id="dateOfBirth" name="dateOfBirth" className="form-input w-3/4 text-black  h-10" />
             </div>
 
             <div className="mb-4 flex justify-between">
               <label htmlFor="age" className="text-sm font-medium w-1/4">
                 Age:
               </label>
-              <input type="number" id="age" name="age" className="form-input w-3/4 text-black" />
+              <input type="number" id="age" name="age" className="form-input w-3/4 text-black h-10" />
             </div>
 
             <div className="mb-4 flex justify-between">
               <label htmlFor="homeAddress" className="text-sm font-medium w-1/4">
                 Home Address:
               </label>
-              <input type="text" id="homeAddress" name="homeAddress" className="form-input w-3/4 text-black" />
+              <input type="text" id="homeAddress" name="homeAddress" className="form-input w-3/4 text-black  h-10" />
             </div>
 
             <div className="mb-4 flex justify-between">
               <label htmlFor="contactPhone" className="text-sm font-medium w-1/4">
                 Contact Phone No.:
               </label>
-              <input type="tel" id="contactPhone" name="contactPhone" className="form-input w-3/4 text-black" />
+              <input type="tel" id="contactPhone" name="contactPhone" className="form-input w-3/4 text-black  h-10" />
             </div>
 
             <div className="mb-4 flex justify-between">
               <label htmlFor="emailAddress" className="text-sm font-medium w-1/4">
                 Email Address:
               </label>
-              <input type="email" id="emailAddress" name="emailAddress" className="form-input w-3/4 text-black" />
+              <input type="email" id="emailAddress" name="emailAddress" className="form-input w-3/4 text-black  h-10" />
             </div>
 
             <h2 className="text-2xl font-semibold mb-4">Education</h2>
             <div className="mb-4 flex justify-between items-center">
               <label className="text-sm font-medium w-1/4">BECE:</label>
               <input type="checkbox" id="bece" name="bece" className="form-checkbox h-6 w-6" />
-              <input type="text" id="beceSchool" name="beceSchool" className="form-input w-3/4 text-black" placeholder="Name of School" />
+              <input type="text" id="beceSchool" name="beceSchool" className="form-input w-3/4 text-black  h-10" placeholder="Name of School" />
             </div>
 
             <div className="mb-4 flex justify-between items-center">
               <label className="text-sm font-medium w-1/4">WASSCE:</label>
               <input type="checkbox" id="wassce" name="wassce" className="form-checkbox h-6 w-6" />
-              <input type="text" id="wassceSchool" name="wassceSchool" className="form-input w-3/4 text-black" placeholder="Name of School" />
+              <input type="text" id="wassceSchool" name="wassceSchool" className="form-input w-3/4 text-black  h-10" placeholder="Name of School" />
             </div>
 
             <div className="mb-4 flex justify-between items-center">
               <label className="text-sm font-medium w-1/4">High School Diploma:</label>
               <input type="checkbox" id="highSchoolDiploma" name="highSchoolDiploma" className="form-checkbox h-6 w-6" />
-              <input type="text" id="highSchoolDiplomaSchool" name="highSchoolDiplomaSchool" className="form-input w-3/4 text-black" placeholder="Name of School" />
+              <input type="text" id="highSchoolDiplomaSchool" name="highSchoolDiplomaSchool" className="form-input w-3/4 text-black  h-10" placeholder="Name of School" />
             </div>
 
             <div className="mb-4 flex justify-between items-center">
               <label className="text-sm font-medium w-1/4">Degree:</label>
               <input type="checkbox" id="degree" name="degree" className="form-checkbox h-6 w-6" />
-              <input type="text" id="degreeSchool" name="degreeSchool" className="form-input w-3/4 text-black" placeholder="Name of School" />
+              <input type="text" id="degreeSchool" name="degreeSchool" className="form-input w-3/4 text-black  h-10" placeholder="Name of School" />
             </div>
 
             <div className="mb-4 flex justify-between items-center">
               <label className="text-sm font-medium w-1/4">HND:</label>
               <input type="checkbox" id="hnd" name="hnd" className="form-checkbox h-6 w-6" />
-              <input type="text" id="hndSchool" name="hndSchool" className="form-input w-3/4 text-black" placeholder="Name of School" />
+              <input type="text" id="hndSchool" name="hndSchool" className="form-input w-3/4 text-black  h-10" placeholder="Name of School" />
             </div>
 
             <div className="mb-4 flex justify-between items-center">
               <label className="text-sm font-medium w-1/4">Masters:</label>
               <input type="checkbox" id="masters" name="masters" className="form-checkbox h-6 w-6" />
-              <input type="text" id="mastersSchool" name="mastersSchool" className="form-input w-3/4 text-black" placeholder="Name of School" />
+              <input type="text" id="mastersSchool" name="mastersSchool" className="form-input w-3/4 text-black  h-10" placeholder="Name of School" />
                 
             </div>
             <p className="text-sm w-3/4 ml-12 text-gray-600 mt-4">
@@ -115,22 +115,22 @@ const ApplicationForm = () => {
               <label htmlFor="companyName" className="text-sm font-medium w-1/4">
                 Company Name:
               </label>
-              <input type="text" id="companyName" name="companyName" placeholder="a" className="form-input w-3/4 text-black bg-gray" />
+              <input type="text" id="companyName" name="companyName" placeholder="a" className="form-input w-3/4 text-black bg-gray  h-10" />
             </div>
             <div className="mb-4 flex justify-between">
               <label className="text-sm font-medium w-1/4">Years:</label>
-              <input type="text" id="yearsA" name="yearsA" className="form-input w-3/4 text-black" placeholder="a" />
+              <input type="text" id="yearsA" name="yearsA" className="form-input w-3/4 text-black  h-10" placeholder="a" />
              
             </div>
             <div className="mb-4 flex justify-between">
               <label htmlFor="companyName" className="text-sm font-medium w-1/4">
                 Company Name:
               </label>
-              <input type="text" id="companyName" name="companyName" placeholder="b" className="form-input w-3/4  text-black bg-gray" />
+              <input type="text" id="companyName" name="companyName" placeholder="b" className="form-input w-3/4  text-black  h-10 bg-gray" />
             </div>
             <div className="mb-4 flex justify-between">
               <label className="text-sm font-medium w-1/4">Years:</label>
-              <input type="text" id="yearsA" name="yearsA" className="form-input w-3/4 text-black" placeholder="b" />
+              <input type="text" id="yearsA" name="yearsA" className="form-input w-3/4 text-black  h-10" placeholder="b" />
              
             </div>
             <p className="text-sm w-3/4 ml-12 text-gray-600 mt-4">
@@ -155,13 +155,13 @@ const ApplicationForm = () => {
               <label htmlFor="parentName" className="text-sm font-medium w-1/4 mb-4">
                 Name:
               </label>
-              <input type="text" id="parentName" name="parentName" className="form-input w-3/4 text-black" />
+              <input type="text" id="parentName" name="parentName" className="form-input w-3/4 text-black  h-10" />
             </div>
             <div className="mb-4 flex justify-between">
               <label htmlFor="parentPhone" className="text-sm font-medium w-1/4 mb-4">
                 Phone No.:
               </label>
-              <input type="tel" id="parentPhone" name="parentPhone" className="form-input w-3/4 text-black" />
+              <input type="tel" id="parentPhone" name="parentPhone" className="form-input w-3/4 text-black  h-10" />
             </div>
             <h2 className="text-2xl font-semibold mb-4">How did you hear about Us?</h2>
             <div className="mb-4 flex justify-between items-center">
@@ -176,7 +176,7 @@ const ApplicationForm = () => {
               <label htmlFor="heardByName" className="text-sm font-medium w-1/4">
                 Name:
               </label>
-              <input type="text" id="heardByName" name="heardByName" className="form-input w-3/4 text-black" placeholder="Name"  />
+              <input type="text" id="heardByName" name="heardByName" className="form-input w-3/4 text-black  h-10" placeholder="Name"  />
             </div>
             
             <div className="mb-4 flex justify-between items-center">
@@ -206,7 +206,7 @@ const ApplicationForm = () => {
               <label htmlFor="signatureDate" className="text-sm font-medium w-1/4">
                 Date:
               </label>
-              <input type="date" id="signatureDate" name="signatureDate" className="form-input w-3/4 text-black" />
+              <input type="date" id="signatureDate" name="signatureDate" className="form-input w-3/4 text-black  h-10" />
             </div>
             <div className="mt-8">
           <label htmlFor="uploadPicture" className="block text-sm font-medium text-gray-700">
