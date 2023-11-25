f36ab7bed2352853b3cf04ea203b539669045400
Selormey Hubert <selormeyh@gmail.com>
2023-11-25 08:57:57 +0000

    german language update

--- src/components/German.jsx
+++ src/components/German.jsx
@@ -20,10 +20,14 @@ const German = () => {
       </div>
 
       <div className="flex flex-wrap gap-6 justify-center text-black">
-        <GermanCard img={a7} title="A1" description="" />
-        <GermanCard img={a5} title="A2" description="" />
-        <GermanCard img={b4} title="B1" description="" />
-        <GermanCard img={a2} title="B2" description="" />
+        <GermanCard img={a7} title="A1" description="A1 German for Beginners.Shows the acquisition of basic  language skills in consonance with the CEFR." />
+        <GermanCard img={a5} title="A2" description="A2 German for learners.Shows the acquisition of elementary  language skills  in consonance with the CEFR.
+
+" />
+        <GermanCard img={b4} title="B1" description=" B1 represents the  first part of an intermediate  level of the  German language skills acquisition as  spelt out by the CEFR.
+
+" />
+        <GermanCard img={a2} title="B2" description=" B2 represents the final part of an intermediate  level of the  German language skills acquisition as outlined by the CEFR." />
       </div>
       <CareerPath />
     </div>
